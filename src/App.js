bed6c325b914c8272bb9fb375d304bf58ebd85bf
AbelMuro <abelmuro93@gmail.com>
2024-08-06 20:30:21 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Components/Header';
+import Courses from './Components/Courses';
 import './styles.css';
 
 function App() {
     return(
         <main>
             <Header/>
+            <Courses/>
         </main>
     )
 }
---
+++ src/Components/Courses/icons/index.js
@@ -0,0 +1,13 @@
+import animation from './icon-animation.svg';
+import business from './icon-business.svg';
+import crypto from './icon-crypto.svg';
+import design from './icon-design.svg';
+import photography from './icon-photography.svg';
+
+export default {
+    animation,
+    business,
+    crypto, 
+    design,
+    photography,
+}
